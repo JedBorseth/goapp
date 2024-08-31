d02d71d2d5e9b05d78318125eedf31dd58517746
Jed Borseth <jedborseth@Jeds-MacBook-Pro.local>
2024-08-31 14:34:21 -0700

    public folder & 404 page

--- main.go
+++ main.go
@@ -3,16 +3,63 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"text/template"
 )
 
+
+func main() { 
+	go tailwindMinify()
+    http.HandleFunc("/", root)
+    http.HandleFunc("/css", css)
+    http.HandleFunc("/headers", headers)
+	http.HandleFunc("/login", login)
+	fmt.Println("Server started at port 8090")
+	
+	dir := "public"
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println("Failed to read", dir, err)
+	}
+	for _, file := range files {
+		if(!file.IsDir()){
+			filePath := filepath.Join(dir, file.Name())
+			fmt.Println("Serving", filePath)
+			http.HandleFunc("/public/" + file.Name(), func(w http.ResponseWriter, req *http.Request) {
+				http.ServeFile(w, req, filePath)
+			})
+
+	}
+}
+	
+
+
+    http.ListenAndServe(":8080", nil)
+}
+
 func root(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+        errorHandler(w, req, http.StatusNotFound)
+        return
+    }
 	index := template.Must(template.ParseFiles("index.html"))
 	index.Execute(w, nil)
 }
+func login(w http.ResponseWriter, req *http.Request) {
+	// index := template.Must(template.ParseFiles("login.html"))
+	// index.Execute(w, nil)
+	fmt.Fprint(w, "login")
+}
 func css(w http.ResponseWriter, req *http.Request) {
 	http.ServeFile(w, req, "styles/output.css")
+}
+func errorHandler(w http.ResponseWriter, req *http.Request, status int) {
+    w.WriteHeader(status)
+    if status == http.StatusNotFound {
+        fmt.Fprint(w, "custom 404")
+    }
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
@@ -36,14 +83,3 @@ func tailwindMinify() {
 	fmt.Println(string(stdout))
 }
 
-func main() { 
-	fmt.Println("Server started at port 8090")
-	tailwindMinify()
-	
-    http.HandleFunc("/", root)
-
-    http.HandleFunc("/css", css)
-    http.HandleFunc("/headers", headers)
-
-    http.ListenAndServe(":8090", nil)
-}
